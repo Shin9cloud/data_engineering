951bc4eed0e5aa2029756e5def8ecc27f78c3092
PalGuPyePyeGi <luvkbstar@gmail.com>
2021-04-22 16:16:59 +0900

    numpy, matplotlib test

---
+++ test.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+np.random.seed(0)
+a = np.random.rand(7)
+print(a)
+
+np.random.seed(0)
+a = np.random.rand(7)
+print(a)
+
+c = np.random.rand(7)
+print(c)
+np.random.seed(3)
+c = np.random.rand(7)
+print(c)
